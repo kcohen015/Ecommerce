773576020e38da5b68b34e3e3e7daf66474b0588
katie <kcohen015@gmail.com>
2022-06-12 15:51:44 -0400

    update product class

---
+++ src/main/java/product.java
@@ -0,0 +1,116 @@
+public class product {
+    //data members
+    private String name;
+    private Double price;
+    private String category;
+    private int quantity;
+    private char size;
+    private String author;
+    private String color;
+
+    //constructors
+    public product(String name, Double price, String category, int quantity, char size, String color) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.quantity = quantity;
+        this.size = size;
+        this.color = color;
+    }
+
+    public product(String name, Double price, String category, int quantity, String author) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.quantity = quantity;
+        this.author = author;
+    }
+
+    public product(String name, Double price, String category, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.quantity = quantity;
+    }
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
+        this.size = size;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    //methods
+    private boolean buy(){
+        int quant = getQuantity();
+        if ( quant > 0){
+            quant--;
+            return true;
+        } else
+            return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return "product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", quantity=" + quantity +
+                ", size=" + size +
+                ", author='" + author + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
